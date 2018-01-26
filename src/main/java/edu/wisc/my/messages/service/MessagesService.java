35ad5bfc389e9f58a355c1c4b58880a59c85705f
Doug Reed <doug.reed@wisc.edu>
2018-01-26 13:26:14 -0600

    update deprecated utility method

--- src/main/java/edu/wisc/my/messages/service/MessagesService.java
+++ src/main/java/edu/wisc/my/messages/service/MessagesService.java
@@ -1,20 +1,17 @@
 package edu.wisc.my.messages.service;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-
-import org.apache.commons.io.IOUtils; 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class MessagesService {
@@ -30,8 +27,7 @@ public class MessagesService {
         try{
             Resource resource = resourceLoader.getResource(env.getProperty("message.source"));
             InputStream is = resource.getInputStream();
-            String jsonTxt = IOUtils.toString( is );
-            BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            String jsonTxt = IOUtils.toString( is, "UTF-8" );
             JSONObject json = new JSONObject(jsonTxt);
         
            return json;

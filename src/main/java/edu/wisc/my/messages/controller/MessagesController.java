ba916442b61ad631bb0ce8c0c6ce9c94ee1f078b
Doug Reed <doug.reed@wisc.edu>
2018-01-26 13:27:44 -0600

    adds endpoint for date filtering

--- src/main/java/edu/wisc/my/messages/controller/MessagesController.java
+++ src/main/java/edu/wisc/my/messages/controller/MessagesController.java
@@ -1,18 +1,22 @@
 package edu.wisc.my.messages.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.json.JSONObject;
-import edu.wisc.my.messages.service.MessagesService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wisc.my.messages.model.MessageArray;
+import edu.wisc.my.messages.model.Message;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.wisc.my.messages.service.MessagesService;
 
 @Controller
 public class MessagesController {
@@ -20,7 +24,7 @@ public class MessagesController {
     private MessagesService messagesService;
     
     @RequestMapping(value="/messages", method=RequestMethod.GET)
-    public @ResponseBody void getJson(HttpServletRequest request,
+    public @ResponseBody void getAllMessages(HttpServletRequest request,
         HttpServletResponse response) {
             JSONObject json = messagesService.getRawMessages();
             response.setContentType("application/json");
@@ -31,6 +35,33 @@ public class MessagesController {
                 response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             }
         }
+
+    @RequestMapping(value="/datedMessages", method=RequestMethod.GET)    
+    public @ResponseBody void getDatedMessages(HttpServletRequest request,
+      HttpServletResponse response) {
+        response.setContentType("application/json");
+        JSONObject rawMessages = messagesService.getRawMessages();
+        JSONObject validMessages = new JSONObject();
+        JSONArray  validMessageArray = new JSONArray();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try{
+            MessageArray messageArray = mapper.readValue(rawMessages.toString(), MessageArray.class);
+            for(Message message:messageArray.getMessages()) {
+                if(message.isValidToday()) {
+                    JSONObject validMessage = new JSONObject(mapper.writeValueAsString(message));
+                    validMessageArray.put(validMessage);
+                }
+            }
+            validMessages.put("messages", validMessageArray);
+            response.getWriter().write(validMessages.toString());
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e) {
+            logger.warn("Date filter failure " + e.getMessage());
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @RequestMapping("/")
     public @ResponseBody
     void index(HttpServletResponse response) {
